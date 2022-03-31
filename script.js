a256aa6cc1b16568dcd341a05d4f8f2146f332f9
Javier R <javierresines@gmail.com>
2022-03-30 19:27:24 -0600

    getting html elements in the javascript file

--- script.js
+++ script.js
@@ -1,36 +1,14 @@
-//making the a variable for every card (for every div)
-var card0 = document.querySelector(".card0");
-var card1 = document.querySelector(".card1");
-var card2 = document.querySelector(".card2");
-var card3 = document.querySelector(".card3");
-var card4 = document.querySelector(".card4");
-var card5 = document.querySelector(".card5");
+//getting the question text from html
+const innerquestion = document.querySelector(".innerQuestion");
 
-//making variables for click events
-var choice0 = document.querySelector(".choice0")
-var choice1 = document.querySelector(".choice1")
-var choice2 = document.querySelector(".choice2")
-var choice3 = document.querySelector(".choice3")
-var choice4 = document.querySelector(".choice4")
-var choice5 = document.querySelector(".choice5")
+//getting the answer choices from html
+const answers = Array.from (document.querySelectorAll("answertext");
 
-// formulas to display next question and hide the previous question
-choice0.addEventListener("click", function(){
-card0.setAttribute("style", "display:none");
-card1.setAttribute("style", "display:contents");})
+//getting the score from the html
+const scoreinscreen = document.querySelector(".scoretext")
 
-choice1.addEventListener("click", function(){
-    card1.setAttribute("style", "display:none");
-    card2.setAttribute("style", "display:contents");})
+//getting the time countdown from the html
 
-choice2.addEventListener("click", function(){
-    card2.setAttribute("style", "display:none");
-    card3.setAttribute("style", "display:contents");})
+const timeelement = document.querySelector(".timeleft")
 
-choice3.addEventListener("click", function(){
-    card3.setAttribute("style", "display:none");
-    card4.setAttribute("style", "display:contents");})
-
-choice4.addEventListener("click", function(){
-    card4.setAttribute("style", "display:none");
-    card5.setAttribute("style", "display:contents");})
+//////////////////////////////////////////////
